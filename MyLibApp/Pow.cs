575cc795bb34cf44ca9e4340e63f72372bb1884d
joshua.roasa19@gmail.com <70301998+aauhsoj19@users.noreply.github.com>
2023-02-03 13:26:30 -0500

    Add project files.

---
+++ MyLibApp/Pow.cs
@@ -0,0 +1,18 @@
+﻿//Joshua Roasa, Molisha Khosla, Angel Robles
+namespace MyLibApp
+{
+    public class Pow
+    {
+        public static double pow(double num1, double num2)
+        {
+            double result_one = Math.Pow(num1, num2);
+            return result_one;
+        }
+        public static double pow(double num1, double num2, double num3)
+        {
+            double result_one = Math.Pow(num1, num2);
+            double result_two = Math.Pow(result_one, num3);
+            return result_two;
+        }
+    }
+}
---
+++ TestMyLipApp/Class1.cs
@@ -0,0 +1,27 @@
+﻿//Joshua Roasa, Molisha Khosla, Angel Robles
+using static System.Console;
+
+namespace TestMyLipApp
+
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            Double numOne, numTwo, numThree;
+
+            Write("Enter your first number: ");
+            numOne = Convert.ToDouble(ReadLine());
+            Write("Enter your second number: ");
+            numTwo = Convert.ToDouble(ReadLine());
+                    
+            WriteLine("{0} to the power of {1} is {2} ", numOne, numTwo, MyLibApp.Pow.pow(numOne, numTwo));
+
+            Write("Enter your third number: ");
+            numThree = Convert.ToDouble(ReadLine());
+
+            WriteLine("{0} to the power of {1} is {2} ", MyLibApp.Pow.pow(numOne, numTwo), numThree, MyLibApp.Pow.pow(numOne, numTwo, numThree));
+        }
+    }
+}
